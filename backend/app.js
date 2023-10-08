39d6c7aed5c6d424e353d4bb5687c9166a24d8b4
karin_1k <117400438+Karin-1k@users.noreply.github.com>
2023-10-08 13:43:06 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-console.log('shta xoshakan');
-console.log(process.env.APPPORT);
- console.log(process.env.HOST); 
- console.log(process.env.DATABASE); 
-console.log(process.env.USER);
-console.log(process.env.PASSWORD);
-console.log(process.env.port);
-console.log(process.env.TOKEN);
-console.log(process.env.ALLPRODUCTS);
-console.log(process.env.REGISTER);
-console.log(process.env.LOGIN);
-console.log(process.env.INFO);
+
 
 var server = app.listen(process.env.APPPORT || 3000, () => {
     console.log('the server created');
