94d80b1be0a1e6070b5c979df243cd5646b60595
karin_1k <117400438+Karin-1k@users.noreply.github.com>
2023-10-08 13:24:32 +0300

    Update mysql.js

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 var db = mysql.createConnection({
-    "host": process.env.HOST,
-    "database": process.env.DATABASE,
-    "user": process.env.USER,
-    "password": process.env.PASSWORD,
-    "port": process.env.PORT
+    "host":bzqf142wakbeyktl5tf6-mysql.services.clever-cloud.com,
+    "database": bzqf142wakbeyktl5tf6,
+    "user":ufvayocwkmsptg1p,
+    "password":OkgpQ10Q4qj5tLrNgXX0,
+    "port":3306
 });
 
 if (db.connect) {
